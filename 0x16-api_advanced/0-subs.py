1ca9aa35b3c2eb19c9ff9f8b8beedf025d2f421a
mohamed1891 <ma.elmoneim1991@gmail.com>
2024-06-30 21:26:26 +0300

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and
-returns the number of subscribers."""
-import requests
+"""
+number of subscribers for a given subreddit
+"""
+
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subs, 0 if the given subreddit is invalid."""
-    headers = {"User-Agent": "youBot/1.0"}
-    r = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
-        headers=headers,
-        allow_redirects=False,
-    )
-    if r.status_code == 200:
-        data = r.json()
-        subs = data["data"]["subscribers"]
-        return subs
-    return 0
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
